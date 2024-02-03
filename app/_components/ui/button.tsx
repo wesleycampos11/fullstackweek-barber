d8dcd2961d40e54541b3c8fc300f429e70a2cbda
Wesley Campos Silva <wesleyreus2020@gmail.com>
2024-02-03 11:51:22 -0300

    commit

--- app/_components/ui/button.tsx
+++ app/_components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-indigo-600 text-primary",
+        default: "bg-indigo-600 text-white",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
